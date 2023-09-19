83a863fd6b4dcb66d4171cb8cee47a33a54f8a4d
ARTEMSHENDRYK <artemshendryk@gmail.com>
2023-09-19 16:20:16 +0300

    Update recommendedFood.js

--- controllers/recommendedFood.js
+++ controllers/recommendedFood.js
@@ -2,8 +2,6 @@ const { RecommendedFood } = require("../models/recommendedFood");
 
 async function getAll(req, res, next) {
   const docs = await RecommendedFood.find({}).exec();
-  console.log(docs);
-  console.log(req.params);
   return res.json(docs);
 }
 
