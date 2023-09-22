dc8c2b92a9eee4069dc84245aaed77a29b4f7769
Albina Yevtushenko <109302599+albinayevtushenko@users.noreply.github.com>
2023-09-22 18:30:14 +0300

    add swagger json file

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-
-const authRouter = require('./routes/api/auth');
+const authRouter = require("./routes/api/auth");
 const foodRouter = require("./routes/api/food");
 const waterRouter = require("./routes/api/water");
 const userRouter = require("./routes/api/user");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
@@ -23,14 +24,15 @@ app.use("/api/user", foodRouter);
 app.use("api/user", waterRouter);
 app.use("/api/user", userRouter);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
-
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
-  module.exports = app;
+module.exports = app;
